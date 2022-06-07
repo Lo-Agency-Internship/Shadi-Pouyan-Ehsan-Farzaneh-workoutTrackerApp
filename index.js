3efdededeae93937f8883af6fbb8ee700283db96
pouyanze <pouyan.ze@gmail.com>
2022-06-07 17:46:29 +0430

    edit files to use twig and macro

--- index.js
+++ index.js
@@ -9,6 +9,13 @@ const {load , save} = require("./utils");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
 
+app.set("view engine", "twig")
+app.set("views", path.join(__dirname, "src", "pages"))
+
+app.get("/edit", (req, res) => {
+  res.render("edit.twig")
+})
+
 app.listen(port, () => {
   console.log(`The server is running succesfully on the http://localhost:${port}. `);
 });
