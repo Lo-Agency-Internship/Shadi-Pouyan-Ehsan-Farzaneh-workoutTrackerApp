7c997b1c3b53ee5bd42a8c122d3610e4f07d288c
pouyanze <pouyan.ze@gmail.com>
2022-06-12 17:43:39 +0430

    add logic to add info from page edit to DB

---
+++ SRC/database/constants.js
@@ -0,0 +1,19 @@
+module.exports = {
+  CREATE_SAMPLE_TABLE: `CREATE TABLE IF NOT EXISTS excercises(
+    id INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE,
+    taskName text,
+    subTaskName text,
+    roundRange text,
+    timeRange text,
+    description text,
+    date text
+    )`,
+  INSERT_NEW_SAMPLE: `INSERT INTO excercises (
+    taskName,
+    subTaskName,
+    roundRange,
+    timeRange,
+    description,
+    date)
+    VALUES(?,?,?,?,?,CURRENT_DATE)`,
+};
--- index.js
+++ index.js
@@ -1,25 +1,99 @@
 const express = require("express");
-const { parse } = require("path");
-const port = 3000;
-const path = require("path");
 const app = express();
-const {load , save} = require("./utils");
-
-
 app.use(express.json());
-app.use(express.urlencoded({ extended: true}));
+app.use(express.urlencoded({ extended: true }));
+const path = require("path");
+const constants = require("./src/database/constants")
+// app.use('/static', express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, "src", "public")));
 
-app.set("view engine", "twig")
-app.set("views", path.join(__dirname, "src", "pages"))
+const { parse } = require("path");
+
+const port = 3000;
+
+const { load, save } = require("./utils");
+
+const axios = require("axios").default;
+
+app.set("view engine", "twig");
+app.set("views", path.join(__dirname, "src", "pages"));
+
+// =======================================================
+// Database
+// =======================================================
+
+const Database = require("better-sqlite3");
+const db = new Database(
+  "src/database/excercises.db",
+  Database.OPEN_READWRITE,
+  (err) => {
+    if (err) {
+      console.error(err.message);
+    }
+    verbose: console.log;
+  }
+);
+function prepare() {
+  const statement = db.prepare(constants.CREATE_SAMPLE_TABLE);
+  const info = statement.run();
+  console.log(info);
+}
+/**
+ * @param {string} taskName
+ * @param {string} subTaskName
+ * @param {string} roundRange
+ * @param {string} timeRange
+ * @param {string} description
+ */
+function insert(taskName, subTaskName, roundRange, timeRange, description) {
+  const statement = db.prepare(constants.INSERT_NEW_SAMPLE);
+  const info = statement.run(taskName, subTaskName, roundRange, timeRange, description);
+  console.log(info);
+}
+
+// function autoIncrement(id) {
+  
+// }
+// db.aggregate('addAll', {
+//   start: 0,
+//   step: (total, nextValue) => total + nextValue,
+// });
+
+// db.prepare('SELECT addAll(dollars) FROM expenses').pluck().get(); // => 92
+
+
+// =================================================================
+// routes
+// =================================================================
 
 app.get("/edit", (req, res) => {
-  res.render("edit.twig")
-})
+  res.render("edit.twig");
+});
+
+app.post("/edit/api", (req, res) => {
+  const excercise = req.body.excerciseName.split(",")[0];
+  const subExcercise = req.body.excerciseName.split(",")[1];
+  const roundRange = req.body.roundRange;
+  const timeRange = req.body.timeRange;
+  const description = req.body.description;
+  
+
+  prepare();
+  insert(excercise, subExcercise, roundRange, timeRange, description);
+
+});
+
+// =====================================
+// =====================================
 
 app.get("/homepage", (req, res) => {
-  res.render("homepage.twig")
-})
+  res.render("homepage.twig");
+});
 
+// =====================================
+// =====================================
 app.listen(port, () => {
-  console.log(`The server is running succesfully on the http://localhost:${port}. `);
+  console.log(
+    `The server is running succesfully on the http://localhost:${port}. `
+  );
 });
