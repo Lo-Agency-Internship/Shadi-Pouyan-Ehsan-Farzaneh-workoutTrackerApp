e0b1742bde327648df879d5e16b0a3c94ca30522
pouyanze <pouyan.ze@gmail.com>
2022-06-15 16:45:00 +0430

    cut db operations

--- index.js
+++ index.js
@@ -1,24 +1,12 @@
 const express = require("express");
-const app = express();
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 
 const path = require("path");
 
-const constants = require("./src/database/constants");
-// app.use('/static', express.static(path.join(__dirname, 'public')))
-app.use(express.static(path.join(__dirname, "src", "public")));
-
 const { parse } = require("path");
-
-const port = 3000;
 
 const { load, save } = require("./utils");
 
 const axios = require("axios").default;
-
-app.set("view engine", "twig");
-app.set("views", path.join(__dirname, "src", "pages"));
 
 const fs = require("fs");
 
@@ -27,70 +15,45 @@ const fs = require("fs");
 // =======================================================
 
 const Database = require("better-sqlite3");
-const db = new Database(
-  "src/database/database.db",
-  Database.OPEN_READWRITE,
-  (err) => {
-    if (err) {
-      console.error(err.message);
-    }
-    verbose: console.log;
-  }
-);
 
-// ========================Functions===============================
+const { initializedDB, Ldb } = require("./src/database/initialDataBase");
+initializedDB();
 
-function prepare() {
-  const statement = db.prepare(constants.CREATE_SAMPLE_TABLE);
-  const info = statement.run();
-  console.log(info);
-}
-/**
- * @param {string} taskName
- * @param {string} subTaskName
- * @param {string} roundRange
- * @param {string} timeRange
- * @param {string} description
- */
+// =======================================================
 
-function insert(taskName, subTaskName, roundRange, timeRange, description) {
-  const statement = db.prepare(constants.INSERT_NEW_SAMPLE);
-  const info = statement.run(
-    taskName,
-    subTaskName,
-    roundRange,
-    timeRange,
-    description
-  );
-  console.log(info);
-}
+const {
+  prepareDB,
+  insert,
+  deleteExercise,
+  loadDataBase,
+} = require("./src/database/dbOperations");
 
-function loadDataBase() {
-  
-  const statement = db.prepare(constants.LOAD_DATABASE);
-  
-  const info = statement.all();
-  
-  return info;
-}
+const app = express();
 
-function deleteExercise(id) {
-  
-  const statement = db.prepare(constants.DELETE_EXERCISE);
-  
-  const info = statement.run(id);
-  
-  console.log(info);
-}
+app.use(express.json());
 
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static(path.join(__dirname, "src", "public")));
+
+const port = 3000;
+
+app.set("view engine", "twig");
+app.set("views", path.join(__dirname, "src", "pages"));
 
 // =================================================================
 // routes
 // =================================================================
 
+// =================================================================
+// add exercise
+// =================================================================
+
 app.get("/add", (req, res) => {
   res.render("add.twig");
 });
+
+// =================================
 
 app.post("/add/api", (req, res) => {
   const excercise = req.body.excerciseName.split(",")[0];
@@ -99,39 +62,41 @@ app.post("/add/api", (req, res) => {
   const timeRange = req.body.timeRange;
   const description = req.body.description;
 
-  prepare();
+  prepareDB();
+
   insert(excercise, subExcercise, roundRange, timeRange, description);
 });
 
-// =====================================
-// =====================================
+// =================================================================
+// homepage
+// =================================================================
 
 app.get("/homepage", (req, res) => {
-
   const path = "./src/database/database.db";
   try {
     if (fs.existsSync(path)) {
       const exercises = loadDataBase();
-      res.render("./homepage",{
-        exercises
-    })
+      res.render("./homepage", {
+        exercises,
+      });
     }
   } catch (err) {
     res.render("information");
   }
-  
 });
 
-// ===============================
-// ===============================
+// =================================
+
 app.post("/homepage/delete", (req, res) => {
-  const exercise= req.body;
+  const exercise = req.body;
   deleteExercise(exercise.exerciseID);
-  res.redirect("/homepage")
+  res.redirect("/homepage");
 });
 
-// =====================================
-// =====================================
+// =================================================================
+// listen port
+// =================================================================
+
 app.listen(port, () => {
   console.log(
     `The server is running succesfully on the http://localhost:${port}.`
